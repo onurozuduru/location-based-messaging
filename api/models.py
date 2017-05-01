2c003f840284e490dafcf318b807a602de2d75fa
Onur Ozuduru <onur.ozuduru@gmail.com>
2017-05-01 18:40:49 +0300

    messages_found table sends details

--- api/models.py
+++ api/models.py
@@ -3,10 +3,17 @@ from flask_login import UserMixin
 from config import db
 
 
+class MessagesFound(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    message_id = db.Column(db.Integer, db.ForeignKey('messages.id'))
+
+
 class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(), unique=True, nullable=False)
     password = db.Column(db.String(), nullable=False)
+    found_messages = db.relationship(MessagesFound, backref=db.backref('user'))
 
 
 class Messages(db.Model):
@@ -16,12 +23,7 @@ class Messages(db.Model):
     timestamp = db.Column(db.DateTime)
     latitude = db.Column(db.Float)
     longitude = db.Column(db.Float)
-
-
-class MessagesFound(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    message_id = db.Column(db.Integer, db.ForeignKey('messages.id'))
+    found_messages = db.relationship(MessagesFound, backref=db.backref('message'))
 
 
 class Items(db.Model):
